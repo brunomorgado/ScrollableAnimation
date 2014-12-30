03ef939c9dbfef7b84c007ff7890d53bfd5598a9
Bruno Morgado <brunofcmorgado@gmail.com>
2014-12-30 12:23:47 +0000

    Small refactoring

--- ScrollableAnimation/Tests/ScrollableAnimationControllerTests.swift
+++ ScrollableAnimation/Tests/ScrollableAnimationControllerTests.swift
@@ -1,16 +1,16 @@
 //
-//  ScrollableAnimationExampleTests.swift
-//  ScrollableAnimationExampleTests
+//  ScrollableAnimationControllerTests.swift
+//  ScrollableAnimationExample
 //
-//  Created by Bruno Morgado on 28/12/14.
+//  Created by Bruno Morgado on 30/12/14.
 //  Copyright (c) 2014 kocomputer. All rights reserved.
 //
 
 import UIKit
 import XCTest
 
-class ScrollableAnimationExampleTests: XCTestCase {
-    
+class ScrollableAnimationControllerTests: XCTestCase {
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,9 +20,17 @@ class ScrollableAnimationExampleTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
     }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
 }
